72cd1887acf1284bfad3208484c95853d8e775e0
Sanath Ali Shaikh <sheikhsanath12@gmail.com>
2025-12-29 18:20:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7522564713-d4c83",
+  "appId": "1:610612413992:web:c43bba065c41a3b91a171d",
+  "apiKey": "AIzaSyDFD3Fba93ww1bBMk6vjo_RsV4ywDH0qsY",
+  "authDomain": "studio-7522564713-d4c83.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "610612413992"
+};
