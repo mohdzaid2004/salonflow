695dfca037578d5e49f884a089e3ceb153772414
Sanath Ali Shaikh <sheikhsanath12@gmail.com>
2026-01-02 11:12:00 +0000

    instead of dashboard, let the login screen be "/home"

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -57,7 +57,7 @@ export default function LoginPage() {
   // Effect to redirect if user is logged in
   useEffect(() => {
     if (!isUserLoading && user) {
-      router.push('/dashboard');
+      router.push('/home');
     }
   }, [user, isUserLoading, router]);
 
