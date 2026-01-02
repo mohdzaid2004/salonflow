4a84447c95f7c6c21951668ff41ac11ba0fd4176
Sanath Ali Shaikh <sheikhsanath12@gmail.com>
2026-01-02 13:07:22 +0000

    ## Error Type Build Error
    ## Error Message
    Error evaluating Node.js cod

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,16 @@ export default {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/page.tsx',
+    './src/app/layout.tsx',
+    './src/app/home/page.tsx',
+    './src/app/dashboard/layout.tsx',
+    './src/app/dashboard/billing/page.tsx',
+    './src/app/dashboard/customers/page.tsx',
+    './src/app/dashboard/services/page.tsx',
+    './src/app/dashboard/settings/page.tsx',
+    './src/app/dashboard/staff/page.tsx',
+    './src/app/(auth)/**/*.tsx',
   ],
   theme: {
     extend: {
